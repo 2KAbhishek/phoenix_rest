d4c19e680a0c744a1baefc047a9c38246def48fc
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-01-20 17:53:52 +0530

    feat: add user resource to router

--- lib/phoenix_rest_web/router.ex
+++ lib/phoenix_rest_web/router.ex
@@ -9,6 +9,11 @@ defmodule PhoenixRestWeb.Router do
     pipe_through :api
   end
 
+  scope "/api", UsersApiWeb do
+    pipe_through :api
+    resources "/users", UserController
+  end
+
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:phoenix_rest, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
